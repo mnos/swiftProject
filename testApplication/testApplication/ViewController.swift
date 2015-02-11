a7f38cbef1051e1aaadff0ee76f2d2f7034b7878
mnos <matthew.nos@icloud.com>
2015-02-11 14:25:29 -0600

    go

---
+++ testApplication/testApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testApplication
+//
+//  Created by Matthew Nos on 2/11/15.
+//  Copyright (c) 2015 Matthew Nos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // first addendum to code
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
